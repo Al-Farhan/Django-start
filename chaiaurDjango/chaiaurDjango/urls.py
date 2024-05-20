a204890fb5c095010022d4044689afd989d1854b
Farhan Shaikh <95585017+Al-Farhan@users.noreply.github.com>
2024-05-20 18:36:52 +0530

    tailwind css integrate

--- chaiaurDjango/chaiaurDjango/urls.py
+++ chaiaurDjango/chaiaurDjango/urls.py
@@ -23,5 +23,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='home'),
     path('contact/', views.contact, name='home'),
-    path('chai/', include('chai.urls'))
+    path('chai/', include('chai.urls')),
+
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
